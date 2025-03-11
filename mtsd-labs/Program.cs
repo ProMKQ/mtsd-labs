198e63759cb4f8e35f38573f24762d0aa0e42ff1
ProMKQ <maximik128@gmail.com>
2025-03-11 22:26:56 +0200

    Add project files.

---
+++ mtsd-labs/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace mtsd_labs;
+
+internal class Program
+{
+    private static (double, double) SolveQuadratic(double a, double b, double c)
+    {
+        double temp = -0.5 * (b + Math.Sign(b) * Math.Sqrt(b * b - 4 * a * c));
+        double x1 = temp / a;
+        double x2 = c / temp;
+        return (x1, x2);
+    }
+
+    private static void Main(string[] args)
+    {
+        if (args.Length == 3)
+        {
+            double a = double.Parse(args[0]);
+            double b = double.Parse(args[1]);
+            double c = double.Parse(args[2]);
+            (double x1, double x2) = SolveQuadratic(a, b, c);
+            Console.WriteLine($"x1 = {x1}, x2 = {x2}");
+        }
+    }
+}
