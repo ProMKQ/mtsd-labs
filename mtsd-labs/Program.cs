037afe8132c79c3f077a6957cf878c39e609c3b6
ProMKQ <maximik128@gmail.com>
2025-03-14 00:42:12 +0200

    Add file mode

--- mtsd-labs/Program.cs
+++ mtsd-labs/Program.cs
@@ -37,10 +37,10 @@ internal class Program
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 
+        Span<double> coef = stackalloc double[3];
+
         if (args.Length == 0)
         {
-            Span<double> coef = stackalloc double[3];
-
             for (int i = 0; i < coef.Length; i++)
             {
                 while (true)
@@ -48,30 +48,64 @@ internal class Program
                     Console.Write($"{(char)('a' + i)} = ");
                     string? input = ReadLineStyled();
 
-                    if (double.TryParse(input, out coef[i]))
+                    if (double.TryParse(input, out coef[i]) && double.IsFinite(coef[i]))
                     {
                         break;
                     }
                     Console.WriteLine($"Error: '{input}' is not a valid real number.");
                 }
             }
-
-            Console.WriteLine($"Equation: ({coef[0]}) x^2 + ({coef[1]}) x + ({coef[2]}) = 0");
-
-            List<double> roots = SolveQuadratic(coef[0], coef[1], coef[2]);
-            Console.WriteLine(roots.Count == 1 ? $"There is 1 root" : $"There are {roots.Count} roots");
-            for (int i = 0; i < roots.Count; i++)
-            {
-                Console.WriteLine($"x{i + 1} = {roots[i]}");
-            }
         }
         else if (args.Length == 1)
         {
-            // TODO: File mode
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("Error: The file does not exist.");
+                return;
+            }
+
+            List<string> lines = [.. File.ReadLines(args[0]).Take(2)];
+            if (lines.Count != 1)
+            {
+                Console.WriteLine("Error: The file must contain exactly 1 line.");
+                return;
+            }
+
+            string[] parts = lines[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 3)
+            {
+                Console.WriteLine("Error: The file must contain exactly 3 coefficients.");
+                return;
+            }
+
+            for (int i = 0; i < coef.Length; i++)
+            {
+                if (!double.TryParse(parts[i], out coef[i]) || !double.IsFinite(coef[i]))
+                {
+                    Console.WriteLine($"Error: '{parts[i]}' is not a valid real number.");
+                    return;
+                }
+            }
         }
         else
         {
-            Console.WriteLine("Usage: mtsd-labs [file]");
+            Console.WriteLine("Usage: mtsd-labs [filepath]");
+            return;
+        }
+
+        Console.WriteLine($"Equation: ({coef[0]}) x^2 + ({coef[1]}) x + ({coef[2]}) = 0");
+
+        if (coef[0] == 0 && coef[1] == 0 && coef[2] == 0)
+        {
+            Console.WriteLine("There are infinite roots");
+            return;
+        }
+        List<double> roots = SolveQuadratic(coef[0], coef[1], coef[2]);
+
+        Console.WriteLine(roots.Count == 1 ? $"There is 1 root" : $"There are {roots.Count} roots");
+        for (int i = 0; i < roots.Count; i++)
+        {
+            Console.WriteLine($"x{i + 1} = {roots[i]}");
         }
     }
 }
