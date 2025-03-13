952d86201831868c80464d7020dde348e02ed023
ProMKQ <maximik128@gmail.com>
2025-03-13 23:49:06 +0200

    Add interactive mode

--- mtsd-labs/Program.cs
+++ mtsd-labs/Program.cs
@@ -1,24 +1,77 @@
-﻿namespace mtsd_labs;
+﻿using System.Globalization;
+
+namespace mtsd_labs;
 
 internal class Program
 {
-    private static (double, double) SolveQuadratic(double a, double b, double c)
+    private static List<double> SolveQuadratic(double a, double b, double c)
     {
         double temp = -0.5 * (b + Math.Sign(b) * Math.Sqrt(b * b - 4 * a * c));
         double x1 = temp / a;
         double x2 = c / temp;
-        return (x1, x2);
+
+        List<double> roots = [];
+        if (double.IsFinite(x1))
+        {
+            roots.Add(x1);
+        }
+        if (double.IsFinite(x2))
+        {
+            roots.Add(x2);
+        }
+
+        return roots;
+    }
+
+    private static string? ReadLineStyled()
+    {
+        Console.Write("\x1b[32;3m");
+        string? input = Console.ReadLine();
+        Console.Write("\x1b[0m");
+
+        return input;
     }
 
     private static void Main(string[] args)
     {
-        if (args.Length == 3)
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+        if (args.Length == 0)
         {
-            double a = double.Parse(args[0]);
-            double b = double.Parse(args[1]);
-            double c = double.Parse(args[2]);
-            (double x1, double x2) = SolveQuadratic(a, b, c);
-            Console.WriteLine($"x1 = {x1}, x2 = {x2}");
+            Span<double> coef = stackalloc double[3];
+
+            for (int i = 0; i < coef.Length; i++)
+            {
+                while (true)
+                {
+                    Console.Write($"{(char)('a' + i)} = ");
+                    string? input = ReadLineStyled();
+
+                    if (double.TryParse(input, out coef[i]))
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"Error: '{input}' is not a valid real number.");
+                }
+            }
+
+            Console.WriteLine($"Equation: ({coef[0]}) x^2 + ({coef[1]}) x + ({coef[2]}) = 0");
+
+            List<double> roots = SolveQuadratic(coef[0], coef[1], coef[2]);
+            Console.WriteLine(roots.Count == 1 ? $"There is 1 root" : $"There are {roots.Count} roots");
+            for (int i = 0; i < roots.Count; i++)
+            {
+                Console.WriteLine($"x{i + 1} = {roots[i]}");
+            }
+        }
+        else if (args.Length == 1)
+        {
+            // TODO: File mode
+        }
+        else
+        {
+            Console.WriteLine("Usage: mtsd-labs [file]");
         }
     }
 }
